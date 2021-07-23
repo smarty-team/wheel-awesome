701b8d5a281e441eee28354c3f72f7dd304539f3
josephxia <15906475@qq.com>
2021-07-23 17:42:53 +0800

    add helloworld code

---
+++ helloworld/__tests__/index.spec.js
@@ -0,0 +1,20 @@
+describe("Helloworld", () => {
+  test("返回参数正确", () => {
+    const hello = require("../index");
+
+    const fn = jest.fn();
+    hello();
+    expect(hello(fn)).toBe("hello world");
+  });
+
+  test("回调函数被正确调用", () => {
+    const hello = require("../index");
+
+    const fn = jest.fn();
+    hello(fn);
+
+    const calls = fn.mock.calls;
+    expect(calls.length).toBe(1);
+    expect(calls[0][0]).toBe("hi");
+  });
+});
---
+++ helloworld/index.js
@@ -0,0 +1,4 @@
+module.exports = (callback) => {
+    callback && callback('hi')
+    return 'hello world'
+}
